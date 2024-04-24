15d0d682e275444098bdf39e5d48042f8ea4545f
Alan <davidalan0826@gmail.com>
2024-04-24 18:01:33 +0800

    2.2 Bouncing off the Boundary

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -7,9 +7,17 @@ public class Ball : MonoBehaviour
     [SerializeField, Min(0f)]
 	float
 		constantXSpeed = 8f,
-		constantYSpeed = 10f;
+		constantYSpeed = 10f,
+        extents=0.5f;
+
 
 	Vector2 position, velocity;
+
+    public float Extents => extents;
+	
+	public Vector2 Position => position;
+
+    
 
     public void UpdateVisualization () =>
 		transform.localPosition = new Vector3(position.x, 0f, position.y);
@@ -22,6 +30,18 @@ public class Ball : MonoBehaviour
 		position = Vector2.zero;
 		UpdateVisualization();
 		velocity = new Vector2(constantXSpeed, -constantYSpeed);
+	}
+
+    public void BounceX (float boundary)
+	{
+		position.x = 2f * boundary - position.x;
+		velocity.x = -velocity.x;
+	}
+
+	public void BounceY (float boundary)
+	{
+		position.y = 2f * boundary - position.y;
+		velocity.y = -velocity.y;
 	}
 
 
