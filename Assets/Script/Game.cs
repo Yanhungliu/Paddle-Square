86532a8c29a7e72a7fe5afae2de240e06afc95a7
Alan <davidalan0826@gmail.com>
2024-05-29 13:44:07 +0800

    Refactor Game add event driven to  get selected ball

--- Assets/Script/Game.cs
+++ Assets/Script/Game.cs
@@ -1,163 +1,174 @@
-using System.Collections;
-using System.Collections.Generic;
-using Script;
-using UnityEngine;
 using TMPro;
-using UnityEngine.Serialization;
+using UnityEngine;
 
-public class Game : MonoBehaviour
+namespace Script
 {
-	[SerializeField]
-	LivelyCamera livelyCamera;
-	
-	[SerializeField]
-	Ball _currentBall ;
-
-	[SerializeField]
-	Paddle bottomPaddle, topPaddle;
-
-    [SerializeField, Min(0f)]
-	Vector2 arenaExtents = new Vector2(10f, 10f);
-
-	[SerializeField, Min(2)]
-	int pointsToWin = 3;
-
-	[SerializeField]
-	TextMeshPro countdownText;
-	
-	[SerializeField, Min(1f)]
-	float newGameDelay = 3f;
-
-	float _countdownUntilNewGame;
-	[SerializeField] 
-	private Transform buttons;
-
-	
-	
-	public void SetCurrentBall(Ball currentBall)
-	{
-		_currentBall = currentBall;
-	}
-
-    void Awake ()
+    public class Game : MonoBehaviour
     {
-	    _countdownUntilNewGame = newGameDelay;
-	    PauseToChoose();
+        [SerializeField]
+        LivelyCamera livelyCamera;
+        
+        [SerializeField]
+        Ball currentBall ;
+
+        [SerializeField]
+        Paddle bottomPaddle, topPaddle;
+
+        [SerializeField, Min(0f)]
+        Vector2 arenaExtents = new Vector2(10f, 10f);
+
+        [SerializeField, Min(2)]
+        int pointsToWin = 3;
+
+        [SerializeField]
+        TextMeshPro countdownText;
+        
+        [SerializeField, Min(1f)]
+        float newGameDelay = 3f;
+
+        float _countdownUntilNewGame;
+        [SerializeField] 
+        private Transform buttons;
+
+        void Awake ()
+        {
+            _countdownUntilNewGame = newGameDelay;
+            PauseToChoose();
+            
+            BallSelector.BallSelected += OnBallSelected;
+        }
+
+        void OnDestroy ()
+        {
+            BallSelector.BallSelected -= OnBallSelected;
+        }
+
+        void OnBallSelected(Ball selectedBall)
+        {
+            SetCurrentBall(selectedBall);
+        }
+
+        private void SetCurrentBall(Ball ball)
+        {
+            this.currentBall = ball;
+        }
+
+        void StartNewGame ()
+        {
+            currentBall.StartNewGame();
+            bottomPaddle.StartNewGame();
+            topPaddle.StartNewGame();
+        }
+        
+        void Update ()
+        {
+            bottomPaddle.OnPaddleMove(currentBall.Position.x, arenaExtents.x);
+            topPaddle.OnPaddleMove(currentBall.Position.x, arenaExtents.x);
+
+            if (_countdownUntilNewGame <= 0f)
+            {
+                UpdateGame();
+            }
+            else
+            {
+                UpdateCountdown();
+            }
+        }
+
+        void UpdateGame ()
+        {
+            currentBall.Move();
+            BounceYIfNeeded();
+            BounceXIfNeeded(currentBall.Position.x);
+            currentBall.UpdateVisualization();
+        }
+
+        void UpdateCountdown ()
+        {
+            _countdownUntilNewGame -= Time.deltaTime;
+            if (_countdownUntilNewGame <= 0f)
+            {
+                countdownText.gameObject.SetActive(false);
+                StartNewGame();
+            }
+            else
+            {
+                float displayValue = Mathf.Ceil(_countdownUntilNewGame);
+                if (displayValue < newGameDelay)
+                {
+                    countdownText.SetText("{0}", displayValue);
+                }
+            }
+        }
+
+        void BounceYIfNeeded ()
+        {
+            float yExtents = arenaExtents.y - currentBall.Extents;
+            if (currentBall.Position.y < -yExtents)
+            {
+                BounceY(-yExtents,bottomPaddle,topPaddle);
+            }
+            else if (currentBall.Position.y > yExtents)
+            {
+                BounceY(yExtents,topPaddle,bottomPaddle);
+            } 
+        }
+
+        void BounceY (float boundary, Paddle defender,Paddle attacker)
+        {
+            float durationAfterBounce = (currentBall.Position.y - boundary) / currentBall.Velocity.y;
+            float bounceX = currentBall.Position.x - currentBall.Velocity.x * durationAfterBounce;
+
+            BounceXIfNeeded(bounceX);
+            bounceX = currentBall.Position.x - currentBall.Velocity.x * durationAfterBounce;
+            livelyCamera.PushXZ(currentBall.Velocity);
+            currentBall.BounceY(boundary);
+
+            
+            if (defender.HitBall(bounceX, currentBall.Extents, out float hitFactor))
+            {
+                currentBall.SetXPositionAndSpeed(bounceX, hitFactor, durationAfterBounce);
+            }
+            else 
+            {
+                livelyCamera.JostleY();
+                if (attacker.ScorePoint(pointsToWin))
+                {
+                    EndGame();
+                }
+
+            }
+
+        }
+
+        void EndGame ()
+        {
+            _countdownUntilNewGame = newGameDelay;
+            countdownText.SetText("GAME OVER");
+            countdownText.gameObject.SetActive(true);
+            currentBall.EndGame();
+            PauseToChoose();
+        }
+
+        void BounceXIfNeeded (float x)
+        {
+            float xExtents = arenaExtents.x - currentBall.Extents;
+            if (x < -xExtents)
+            {
+                livelyCamera.PushXZ(currentBall.Velocity);
+                currentBall.BounceX(-xExtents);
+            }
+            else if (x> xExtents)
+            {
+                livelyCamera.PushXZ(currentBall.Velocity);
+                currentBall.BounceX(xExtents);
+            }
+        }
+
+        private void PauseToChoose()
+        {
+            Time.timeScale = 0;
+            buttons.gameObject.SetActive(true);
+        }
     }
-
-    void StartNewGame ()
-	{
-		_currentBall.StartNewGame();
-		bottomPaddle.StartNewGame();
-		topPaddle.StartNewGame();
-	}
-	
-	void Update ()
-	{
-		bottomPaddle.Move(_currentBall.Position.x, arenaExtents.x);
-		topPaddle.Move(_currentBall.Position.x, arenaExtents.x);
-
-	if (_countdownUntilNewGame <= 0f)
-		{
-			UpdateGame();
-		}
-		else
-		{
-			UpdateCountdown();
-		}
-	}
-	void UpdateGame ()
-	{
-		_currentBall.Move();
-		BounceYIfNeeded();
-		BounceXIfNeeded(_currentBall.Position.x);
-		_currentBall.UpdateVisualization();
-	}
-
-	void UpdateCountdown ()
-	{
-		_countdownUntilNewGame -= Time.deltaTime;
-		if (_countdownUntilNewGame <= 0f)
-		{
-			countdownText.gameObject.SetActive(false);
-			StartNewGame();
-		}
-		else
-		{
-			float displayValue = Mathf.Ceil(_countdownUntilNewGame);
-			if (displayValue < newGameDelay)
-			{
-				countdownText.SetText("{0}", displayValue);
-			}
-		}
-	}
-
-    void BounceYIfNeeded ()
-	{
-		float yExtents = arenaExtents.y - _currentBall.Extents;
-		if (_currentBall.Position.y < -yExtents)
-		{
-			BounceY(-yExtents,bottomPaddle,topPaddle);
-		}
-		else if (_currentBall.Position.y > yExtents)
-		{
-			BounceY(yExtents,topPaddle,bottomPaddle);
-		} 
-	}
-	void BounceY (float boundary, Paddle defender,Paddle attacker)
-	{
-		float durationAfterBounce = (_currentBall.Position.y - boundary) / _currentBall.Velocity.y;
-		float bounceX = _currentBall.Position.x - _currentBall.Velocity.x * durationAfterBounce;
-
-		BounceXIfNeeded(bounceX);
-		bounceX = _currentBall.Position.x - _currentBall.Velocity.x * durationAfterBounce;
-		livelyCamera.PushXZ(_currentBall.Velocity);
-		_currentBall.BounceY(boundary);
-
-		
-		if (defender.HitBall(bounceX, _currentBall.Extents, out float hitFactor))
-		{
-			_currentBall.SetXPositionAndSpeed(bounceX, hitFactor, durationAfterBounce);
-		}
-		else 
-		{
-			livelyCamera.JostleY();
-			if (attacker.ScorePoint(pointsToWin))
-			{
-				EndGame();
-			}
-
-		}
-
-	}
-
-	void EndGame ()
-	{
-		_countdownUntilNewGame = newGameDelay;
-		countdownText.SetText("GAME OVER");
-		countdownText.gameObject.SetActive(true);
-		_currentBall.EndGame();
-		PauseToChoose();
-	}
-
-    void BounceXIfNeeded (float x)
-	{
-		float xExtents = arenaExtents.x - _currentBall.Extents;
-		if (x < -xExtents)
-		{
-			livelyCamera.PushXZ(_currentBall.Velocity);
-			_currentBall.BounceX(-xExtents);
-		}
-		else if (x> xExtents)
-		{
-			livelyCamera.PushXZ(_currentBall.Velocity);
-			_currentBall.BounceX(xExtents);
-		}
-	}
-
-	private void PauseToChoose()
-	{
-		Time.timeScale = 0;
-		buttons.gameObject.SetActive(true);
-	}
 }
--- Assets/Script/Paddle.cs
+++ Assets/Script/Paddle.cs
@@ -1,111 +1,109 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
-public class Paddle : MonoBehaviour
+namespace Script
 {
-    [SerializeField]
-	bool isAI;
-
-
-	[SerializeField]
-	TextMeshPro scoreText;
-
-	int score;
-
-
-    [SerializeField, Min(0f)]
-	float
-		minExtents = 4f,
-		maxExtents = 4f,
-		speed = 10f,
-		maxTargetingBias = 0.75f;
-
-	float extents,targetingBias;
-
-
-	void Awake ()
+	public class Paddle : MonoBehaviour
 	{
-		SetScore(0);
-	}
-
-    public void Move (float target, float arenaExtents)
-	{
-		Vector3 p = transform.localPosition;
-        p.x = isAI ? AdjustByAI(p.x, target) : AdjustByPlayer(p.x);
-		float limit = arenaExtents - extents;
-		p.x = Mathf.Clamp(p.x, -limit, limit);
-		transform.localPosition = p;
-	}
+		[SerializeField] 
+		private bool isAI;
 
 
-    float AdjustByAI (float x, float target)//AI Controll Panal
-	{
-		target += targetingBias * extents;
-		if (x < target)
+		[SerializeField] 
+		private TextMeshPro scoreText;
+
+		private int _score;
+
+
+		[SerializeField, Min(0f)]
+		float
+			minExtents = 4f,
+			maxExtents = 4f,
+			speed = 10f,
+			maxTargetingBias = 0.75f;
+
+		private float _extents,_targetingBias;
+
+
+		private void Awake () => SetScore(0);
+
+		public void OnPaddleMove (float target, float arenaExtents)
 		{
-			return Mathf.Min(x + speed * Time.deltaTime, target);
+			Vector3 p = transform.localPosition;
+			p.x = isAI ? AdjustByAI(p.x, target) : AdjustByPlayer(p.x);
+			float limit = arenaExtents - _extents;
+			p.x = Mathf.Clamp(p.x, -limit, limit);
+			transform.localPosition = p;
 		}
-		return Mathf.Max(x - speed * Time.deltaTime, target);
-	}
 
-
-    float AdjustByPlayer (float x)// Player Control Panal
-	{
-		bool goRight = Input.GetKey(KeyCode.RightArrow);
-		bool goLeft = Input.GetKey(KeyCode.LeftArrow);
-		if (goRight && !goLeft)
+		
+		private float AdjustByAI (float x, float target)//AI Controll Panal
 		{
-			return x + speed * Time.deltaTime;
+			target += _targetingBias * _extents;
+			if (x < target)
+			{
+				return Mathf.Min(x + speed * Time.deltaTime, target);
+			}
+			return Mathf.Max(x - speed * Time.deltaTime, target);
 		}
-		else if (goLeft && !goRight)
+
+
+		private float AdjustByPlayer (float x)// Player Control Panal
 		{
-			return x - speed * Time.deltaTime;
+			bool goRight = Input.GetKey(KeyCode.RightArrow);
+			bool goLeft = Input.GetKey(KeyCode.LeftArrow);
+			if (goRight && !goLeft)
+			{
+				return x + speed * Time.deltaTime;
+			}
+			else if (goLeft && !goRight)
+			{
+				return x - speed * Time.deltaTime;
+			}
+			return x;
 		}
-		return x;
-	}
 
 
 
-    public bool HitBall (float ballX, float ballExtents, out float hitFactor)
-	{
-		ChangeTargetingBias();
-		 hitFactor =
-			(ballX - transform.localPosition.x) /
-			(extents + ballExtents);
-		return -1f <= hitFactor && hitFactor <= 1f;
-	}
+		public bool HitBall (float ballX, float ballExtents, out float hitFactor)
+		{
+			ChangeTargetingBias();
+			hitFactor =
+				(ballX - transform.localPosition.x) /
+				(_extents + ballExtents);
+			return hitFactor is >= -1f and <= 1f;
+		}
 
 
-	public void StartNewGame ()
-	{
-		SetScore(0);
-		ChangeTargetingBias();
-	}
+		public void StartNewGame ()
+		{
+			SetScore(0);
+			ChangeTargetingBias();
+		}
 
-	public bool ScorePoint (int pointsToWin)
-	{
-		SetScore(score + 1, pointsToWin);
-		return score >= pointsToWin;
-	}
+		public bool ScorePoint (int pointsToWin)
+		{
+			SetScore(_score + 1, pointsToWin);
+			return _score >= pointsToWin;
+		}
 
-	void SetScore (int newScore, float pointsToWin = 1000f)
-	{
-		score = newScore;
-		scoreText.SetText("{0}", newScore);
-		SetExtents(Mathf.Lerp(maxExtents, minExtents, newScore / (pointsToWin - 1f)));
-	}
+		private void SetScore (int newScore, float pointsToWin = 1000f)
+		{
+			_score = newScore;
+			scoreText.SetText("{0}", newScore);
+			SetExtents(Mathf.Lerp(maxExtents, minExtents, newScore / (pointsToWin - 1f)));
+		}
 
-	void ChangeTargetingBias () =>
-		targetingBias = Random.Range(-maxTargetingBias, maxTargetingBias);
-    
+		private void ChangeTargetingBias () =>
+			_targetingBias = Random.Range(-maxTargetingBias, maxTargetingBias);
 
-	void SetExtents (float newExtents)
-	{
-		extents = newExtents;
-		Vector3 s = transform.localScale;
-		s.x = 2f * newExtents;
-		transform.localScale = s;
+
+		private void SetExtents (float newExtents)
+		{
+			_extents = newExtents;
+			Vector3 s = transform.localScale;
+			s.x = 2f * newExtents;
+			transform.localScale = s;
+		}
 	}
 }
